8e3d0545778892a17484166bfda9f4e4d4cbf6ce
dunn0056@icloud.com <58886408+Sa-wol@users.noreply.github.com>
2021-01-20 07:53:41 -0800

    Added student object for web form

--- ModelsAndModelBinding/Controllers/StudentController.cs
+++ ModelsAndModelBinding/Controllers/StudentController.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ModelsAndModelBinding.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,8 +9,20 @@ namespace ModelsAndModelBinding.Controllers
 {
     public class StudentController : Controller
     {
+        [HttpGet] // When user navigates to URL
         public IActionResult Add()
         {
+            return View();
+        }
+
+        [HttpPost] // Call this Add method after user posts form data
+        public IActionResult Add(IFormCollection form)
+        {
+            Student stu = new Student();
+            stu.FullName = form["full_name"];
+            stu.DateOfBirth = Convert.ToDateTime(form["dob"]);
+            stu.PhoneNumber = form["home_phone"];
+            stu.EmailAddress = form["email"];
             return View();
         }
     }
